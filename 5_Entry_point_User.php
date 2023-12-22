9dc9853d29bfcde94c792b82a39a8b642889f925
sujit <sujitshrestha0004@gmail.com>
2023-12-22 16:41:58 +0545

    Get , Update , CreateUser DONE

--- 5_Entry_point_User.php
+++ 5_Entry_point_User.php
@@ -4,8 +4,9 @@ require_once "./4_DBConnection_ClassBased.php";
 require_once "./5_UserController.php";
 
 $conn = new DbConnection();
+$mysqliConnection = $conn->mysqliConnection();
 
-$obj =  new customerController(new UserModel( $conn ));
+$obj =  new UserController(new UserModel( $mysqliConnection ));
 $obj->requestHandler();
 
 ?>
